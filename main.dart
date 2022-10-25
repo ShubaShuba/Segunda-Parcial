5e2183fc76e970688a573f960c74bb45e5158df1
BG <111468143+ShubaShuba@users.noreply.github.com>
2022-10-25 17:08:14 -0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,106 +1,35 @@
+import 'dart:io';
+//calculadora que lea dos numeros del teclado y obtenga suma, resta, multiplicación y
+//division, usando funciones y asingnado valores a dos variables
 
-
-//operaciones basicas
-//void main(List<String> args) {
-  //var n1 = 15;
-  //var n2 = 7;
-  //print("suma: $n1 + $n2 = ${n1 + n2}");
-  //print("resta: $n1 - $n2 = ${n1 - n2}");
-  //print("multiplicacion: $n1 * $n2 = ${n1 * n2}");
-  //print("division: $n1 / $n2 = ${n1 / n2}");
-  ///print("division entera: $n1 ~/ $n2 = ${n1 ~/ n2}");
-  //print(pow(5,3));
-  //print("minimo: min($n1, $n2) = ${min(n1, n2)}");
-  //print("maximo: max($n1, $n2) = ${max(n1, n2)}");
-  //print("seno: ${sin(45 * pi / 180)}");
-  //print("coseno: ${cos(45 * pi / 180)}");
-  //print("sqrt: ${sqrt(5)}");
-  //print(10.isOdd);
-  //print(10.isEven);
-//}
-
-//Contador positivo
-//void main(List<String> args) {
-  //var contador = 0;
-  //contador = contador + 1;
-  //print(contador);
-  //contador++;
-  //print(contador);
-  //++contador;
-  //print(contador);
-  //contador += 2;
-  //print(contador);
-//}
-//contador negativo
-//void main(List<String> args) {
-  //var contador = 0;
-  //contador = contador + 1;
-  //print(contador);
-  //contador++;
-  //print(contador);
-  //++contador;
-  //print(contador);
-  //contador += 2;
-  //print(contador);
-//}
-
-//multiplicacion con contador
-//void main(List<String> args) {
-  //var contador = 0;
-  //contador *2;
-  //print(contador);
-//} 
-//division con contador
-//void main(List<String> args) {
-  //double contador = 0;
-  //contador %=2;
-  //print(contador);
-//}
-
-//void main(list<String> args) {
-  //const consentero = 10;
-  //var finalentero;
-  //var numero = stdin.readLineSync();
-  //finalentero = numero;
-  //print(numero);
-  //print("el mensaje es : $numero");
-
-  //finalentero = "hola";
-  
-//}
-
-//tiempo de codificacion
-//tiempo de compilacion
-
-
-
-//void main(List<String> args) {
-  //var cadena = 5;
-
-  //print(cadena is String);
-  //print(cadena.runtimeType);
-  //var nombre = "alex";
-  //var edad = 20;
-  //print(nombre  );
-  //print("$nombre $edad");
-//}
-
-
-//void main(List<String> args) {
-  //sistemas numericos
-  //binario
-  //print(125.toRadixString(2));
-  //print(125.toRadixString(8));
-  //print(125.toRadixString(16));
-  //var numero = 0xFFFF;
-  //print(numero.runtimeType);
-//}
-
- void main(List<String> args) {
-  //listas
-  var milista = [1,"hola",9.8,true];
-  print(milista);
-  //agregar elementos a lista
-  milista.add(3.1416);
-  print(milista);
+void main() {
+  String op = leerDatos("Indica la operación [+,-,*,/]");
+  int num1 = int.parse(leerDatos("Dame el primer número"));
+  int num2 = int.parse(leerDatos("Dame el segundo número"));
+  print("${calculadora(op, num1, num2)}");
 }
+
+String leerDatos(String mensaje) {
+  print(mensaje);
+  String data = (stdin.readLineSync()!);
+  return data;
+}
+
+String calculadora(String op, int n1, int n2) {
+  if (op == "+") {
+    return "$n1 + $n2 = ${suma(n1, n2)}";
+  } else if (op == "-") {
+    return "$n1 - $n2 = ${resta(n1, n2)}";
+  } else if (op == "*") {
+    return "$n1 * $n2 = ${multi(n1, n2)}";
+  } else if (op == "/") {
+    return "$n1 / $n2 = ${divi(n1, n2)}";
+  } else {
+    return "Operación inválida";
+  }
+}
+
+int suma(int num1, int num2) => num1 + num2;
+int resta(int num1, int num2) => num1 - num2;
+int multi(int num1, int num2) => num1 * num2;
+int divi(int num1, int num2) => num1 ~/ num2;
